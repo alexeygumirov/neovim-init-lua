5e9ce088cac406b22ce9859a03b6d6c8dba637e8
Alexey Gumirov <ag_devops@die-optimisten.net>
2021-12-19 20:05:23 +0100

    Dashboard key bindings

--- lua/config_modules/dashboard/init.lua
+++ lua/config_modules/dashboard/init.lua
@@ -8,12 +8,12 @@ vim.g.dashboard_custom_header = {
 '╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝       ╚═════╝' ,
 }
 vim.g.dashboard_custom_section = {
-    a = {description = {'  New File           '}, command = 'enew!'},
-    b = {description = {'  Find File          '}, command = 'lua require("telescope.builtin").file_browser({ cwd = require("telescope.utils").buffer_dir() })'},
-    c = {description = {'  Search Text        '}, command = 'lua require("telescope.builtin").live_grep()'},
-    d = {description = {'  Recent Files       '}, command = 'lua require("telescope.builtin").oldfiles()'},
-    e = {description = {'  Git Files          '}, command = 'lua require("alex.telescope").find_files_or_git_files()'},
-    f = {description = {'  VimWiki            '}, command = 'VimwikiIndex'},
-    g = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'},
+    a = {description = {'  New File                        '}, command = 'enew!'},
+    b = {description = {'  Find File                 SPC ff'}, command = 'lua require("telescope.builtin").file_browser({ cwd = require("telescope.utils").buffer_dir() })'},
+    c = {description = {'  Search Text               SPC rr'}, command = 'lua require("telescope.builtin").live_grep()'},
+    d = {description = {'  Recent Files              SPC fo'}, command = 'lua require("telescope.builtin").oldfiles()'},
+    e = {description = {'  Git Files                 SPC gf'}, command = 'lua require("alex.telescope").find_files_or_git_files()'},
+    f = {description = {'  VimWiki                   SPC ww'}, command = 'VimwikiIndex'},
+    g = {description = {'  Config                          '}, command = 'edit ~/.config/nvim/init.lua'},
 }
 vim.g.dashboard_custom_footer = {'Press F2 to call dashboard when needed.'}
