c4fbce5ddb23ec180181980db3c2b122a16af196
Alexey Gumirov <ag_devops@die-optimisten.net>
2022-10-10 23:55:57 +0200

    Lualine format update

--- lua/config_modules/vim_lualine/init.lua
+++ lua/config_modules/vim_lualine/init.lua
@@ -29,11 +29,10 @@ local function get_lsp_name()
     if raw_output ~= nil
     then
         local count = 0
-        for k, v in pairs(raw_output)
-        do
+        for k, v in pairs(raw_output) do
             if count == 0 and v.name ~= nil and v.name ~= ""
             then
-                lsp_server_name = separators.left .. " LSP: " .. v.name
+                lsp_server_name = v.name
             else
                 lsp_server_name = lsp_server_name .. ", " .. v.name
             end
@@ -50,79 +49,81 @@ local function get_lsp_name()
 end
 
 require('lualine').setup({
-  options = {
-    icons_enabled = true,
-    theme = 'nightfly',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-    globalstatus = true,
-  },
-  sections = {
-    lualine_a = {'mode', spellstatus },
-    lualine_b = {
-        'branch',
-        {
-            'filename',
-            path = 2,
-            shorting_path = 60,
-        }
+    options = {
+        icons_enabled = true,
+        theme = 'nightfly',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = true,
     },
-    lualine_c = {
-        { get_lsp_name },
-        -- {
-        --     'buffers',
-        --     show_filenames_only = false,
-        --     show_modified_status = true,
-        --     mode = 1,
-        -- },
-        -- {'filename'},
-    },
-    lualine_x = {
-        'encoding',
-        'fileformat',
-        {
-            'filetype',
-            colored = true,
+    sections = {
+        lualine_a = { 'mode', spellstatus },
+        lualine_b = {
+            'branch',
+            get_lsp_name,
         },
-    },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {
-      lualine_a = {
-        {
-            'buffers',
-            show_filenames_only = true,
-            show_modified_status = true,
-            mode = 0,
-            buffers_color = {
-                -- Same values like general color option can be used here.
-                active = { fg = '#092236', bg =  '#82aaff' }, -- color for active buffer
-                inactive = { fg = '#a1aab8', bg = '#2c3043' },
+        lualine_c = {
+            {
+                'filename',
+                file_status = false,
+                newfile_status = false,
+                path = 3,
+                shorting_path = 10,
+            }
+            -- {
+            --     'buffers',
+            --     show_filenames_only = false,
+            --     show_modified_status = true,
+            --     mode = 1,
+            -- },
+            -- {'filename'},
+        },
+        lualine_x = {
+            'encoding',
+            'fileformat',
+            {
+                'filetype',
+                colored = true,
             },
         },
-      },
-      lualine_b = {'diff',
-                  --{'diagnostics', sources={'nvim_lsp'}}},
-                  {'diagnostics', sources={'nvim_diagnostic'}}},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      -- lualine_z = {'tabs'}
-      lualine_z = {}
-  },
-  extensions = {
-      'fugitive',
-      'fzf',
-  }
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {
+        lualine_a = {
+            {
+                'buffers',
+                show_filenames_only = true,
+                show_modified_status = true,
+                mode = 0,
+                buffers_color = {
+                    -- Same values like general color option can be used here.
+                    active = { fg = '#092236', bg = '#82aaff' }, -- color for active buffer
+                    inactive = { fg = '#a1aab8', bg = '#2c3043' },
+                },
+            },
+        },
+        lualine_b = { 'diff',
+            --{'diagnostics', sources={'nvim_lsp'}}},
+            { 'diagnostics', sources = { 'nvim_diagnostic' } } },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        -- lualine_z = {'tabs'}
+        lualine_z = {}
+    },
+    extensions = {
+        'fugitive',
+        'fzf',
+    }
 })
